68cf33d653cb5a28f38760458420410c459cdb6b
LazyShpee <comemureravaud@gmail.com>
2017-04-18 15:36:17 +0000

    eval config

--- modules/eval.lua
+++ modules/eval.lua
@@ -31,8 +31,9 @@ reset()
 
 local _EVAL = {
   name = 'eval',
-  call = function(m, c, a)
+  call = function(m, c, a, o)
     if #a == 0 then return end
+    --print(pp.strip(pp.dump(o)))
     local tf
 
     if     a:match("^```[^\n]-\n(.*)```$") then       tf = a:match("^```[^\n]-\n(.*)```$")
@@ -41,11 +42,13 @@ local _EVAL = {
     end
 
     local output = {}
+    if not o.persist then reset() end
     sandbox.msg = m
-    sandbox._C = true  -- Format output in markdown block
-    sandbox._S = false -- Silent
-    sandbox._O = true  -- Show result output
-    sandbox._D = false -- Delete eval message
+    sandbox._C = o.code  -- Format output in markdown block
+    sandbox._S = o.silent -- Silent
+    sandbox._O = o.ouput  -- Show result output
+    sandbox._D = o.delete -- Delete eval message
+    sandbox._E = o.error  -- output error log
     sandbox.reset = reset
     sandbox.print = function(...) table.insert(output, printLine(...)) end
     sandbox.p = function(...) table.insert(output, prettyLine(...)) end
@@ -55,23 +58,26 @@ local _EVAL = {
     local fn, syntax = load(tf, '{sandbox.'..c..'}', 't', sandbox)
     if not fn then return m:reply(util.code(syntax)) end
     local success, runtime = pcall(fn)
-    if not success then return m:reply(util.code(runtime)) end
+    if not success then if o.error then return m:reply(util.code(runtime)) else return end end
 
-    if #output == 0 then if sandbox._S then return end return m:reply(util.code('Execution completed.')) end
-    output = table.concat(output, '\n')
-    if sandbox._O then
-      if #output <= 1990 then
-        if sandbox._C then
-          m:reply(util.code(output))
+    if #output == 0 then
+      if not sandbox._S then m:reply(util.code('Execution completed.')) end
+    else
+      output = table.concat(output, '\n')
+      if not sandbox._S and sandbox._O then
+        if #output <= 1990 then
+          if sandbox._C then
+            m:reply(util.code(output))
+          else
+            m:reply(output)
+          end
         else
-          m:reply(output)
-        end
-      else
-        output = output:sub(1, 1980)
-        if sandbox._C then
-          m:reply(util.code(output)..'`[SNIP]`')
-        else
-          m:reply(output..'`[SNIP]`')
+          output = output:sub(1, 1980)
+          if sandbox._C then
+            m:reply(util.code(output)..'`[SNIP]`')
+          else
+            m:reply(output..'`[SNIP]`')
+          end
         end
       end
     end
@@ -90,7 +96,15 @@ Sandbox Variable:
   - msg, the emmited command message object
   - client, egobot client object]],
   author = 'LazyShpee',
-  display_name = 'Lua Eval'
+  display_name = 'Lua Eval',
+  options = {
+    silent = { type = 'toggle', default = false, label = 'Silence any ouput' },
+    error = { type = 'toggle', default = true, label = 'Show error output' },
+    delete = { type = 'toggle', default = false, label = 'Delete command message' },
+    ouput = { type = 'toggle', default = true, label = 'Show output' },
+    code = { type = 'toggle', default = true, label = 'Format output in command block' },
+    persist = { type = 'toggle', default = true, label = 'Persistent run environment' }
+  }
 }
 
 return _EVAL
